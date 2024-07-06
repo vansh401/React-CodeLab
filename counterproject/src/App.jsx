398956f26b5e98e87a992cefe298b4182051e372
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-07-07 02:09:01 +0530

    c

--- counterproject/src/App.jsx
+++ counterproject/src/App.jsx
@@ -10,19 +10,30 @@ function App() {
 // let count=10;
 
 const addValue=()=>{
+  if (count+1>20) {
+    alert("Limit exceeded")
+  }
+  else{
   setCount(count+1)
   console.log("clicked(+1)",count);
+  }
 }
 
 const decValue=()=>{
+  if(count-1<0){
+    alert("Can't be negative")
+  }
+  else{
   setCount(count-1) 
   console.log("clicked(-1)",count);
+  }
 }
   return (
     <>
     <h1>Counter Project</h1>
     <h2>Counter : {count}</h2>
     <button onClick={addValue}>Add Value</button>
+    <br />
     <br />
     <button onClick={decValue}>Decrease Value</button>
     <p>Resulted Value : {count}</p>
